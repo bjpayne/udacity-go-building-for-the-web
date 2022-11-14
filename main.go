7a73042bb073bf3c1af5bbf8f33105d2e9828908
Ben Payne <=>
2022-11-14 16:09:45 -0500

    Serving HTML

--- main.go
+++ main.go
@@ -1,38 +1,26 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-var cityPopulations = map[string]uint32{
-	"Tokyo":       37435191,
-	"Delhi":       29399141,
-	"Shanghai":    26317104,
-	"Sao Paulo":   21846507,
-	"Mexico City": 21671908,
-}
-
-func index(response http.ResponseWriter, request *http.Request) {
-	response.Header().Set("Content-Type", "application/json")
-	response.WriteHeader(http.StatusOK)
-
-	_error := json.NewEncoder(response).Encode(cityPopulations)
-
-	if _error != nil {
-		return
-	}
-}
-
 func main() {
-	http.HandleFunc("/", index)
+	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
+		http.ServeFile(response, request, "./static/index.html")
+	})
+
+	// Handle with an anonymous function
+	http.HandleFunc("/hello", func(response http.ResponseWriter, request *http.Request) {
+		http.ServeFile(response, request, "./static/hello.html")
+	})
 
 	fmt.Println("Server is starting on port 3000...")
-	_error := http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":3000", nil)
 
-	if _error != nil {
-		fmt.Println(_error)
+	if err != nil {
+		fmt.Println(err)
+
 		return
 	}
 }
