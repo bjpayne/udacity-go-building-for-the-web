36087396ec2655f2885fcbe49d96af803533315e
Ben Payne <=>
2022-11-14 15:51:33 -0500

    Building for the web

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 HTTP Fundamentals, net/HTTP Package, Handlers, Serving HTML, Routing
 
-HTTP Fundamentals 1
+HTTP Fundamentals 1 and net/HTTP Package
 
 ```go
 package main
@@ -49,4 +49,52 @@ func main() {
 		return
 	}
 }
+```
+
+Handlers
+
+```go
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+var cityPopulations = map[string]uint32{
+	"Tokyo":       37435191,
+	"Delhi":       29399141,
+	"Shanghai":    26317104,
+	"Sao Paulo":   21846507,
+	"Mexico City": 21671908,
+}
+
+func index(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	response.WriteHeader(http.StatusOK)
+
+	_error := json.NewEncoder(response).Encode(cityPopulations)
+
+	if _error != nil {
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/", index)
+
+	fmt.Println("Server is starting on port 3000...")
+	_error := http.ListenAndServe(":3000", nil)
+
+	if _error != nil {
+		fmt.Println(_error)
+		return
+	}
+}
+```
+
+Serving HTML
+
+```go
 ```
--- main.go
+++ main.go
@@ -1,44 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-var cities = []string{"Tokyo", "Delhi", "Shanghai", "Sao Paulo", "Mexico City"}
-
-func index(response http.ResponseWriter, request *http.Request) {
-	_, err := fmt.Fprintf(response, "Hello World :]")
-
-	if err != nil {
-		return
-	}
+var cityPopulations = map[string]uint32{
+	"Tokyo":       37435191,
+	"Delhi":       29399141,
+	"Shanghai":    26317104,
+	"Sao Paulo":   21846507,
+	"Mexico City": 21671908,
 }
 
-func cityList(response http.ResponseWriter, request *http.Request) {
-	html := "<h1>List of most populous cities</h1>"
+func index(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	response.WriteHeader(http.StatusOK)
 
-	for _, city := range cities {
-		html += fmt.Sprintf("<p>%s</p>", city)
-	}
+	_error := json.NewEncoder(response).Encode(cityPopulations)
 
-	_, err := fmt.Fprintf(response, html)
-
-	if err != nil {
+	if _error != nil {
 		return
 	}
 }
 
 func main() {
-	fmt.Println("Server starting...")
 	http.HandleFunc("/", index)
-	http.HandleFunc("/city-list", cityList)
 
-	err := http.ListenAndServe(":3000", nil)
+	fmt.Println("Server is starting on port 3000...")
+	_error := http.ListenAndServe(":3000", nil)
 
-	if err != nil {
-		fmt.Println(err)
-
+	if _error != nil {
+		fmt.Println(_error)
 		return
 	}
 }
